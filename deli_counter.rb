bbf76dbaeb6fc6b17045a23be6bf2f96b88ae43c
Nicole Stellatos <stellatosnicole@gmail.com>
2020-07-14 21:49:40 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+katz_deli = []
+def line(number)
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  end
+    def take_a_number(katz_deli,customer_name)
+      if katz_deli > 0
+        puts "The line is currently #{katz_deli}, #{custumer_name}."
+      end
+        
+end
+end
