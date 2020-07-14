266aba14d13ee729a72d8155bb765cd3df2dccaa
Nicole Stellatos <stellatosnicole@gmail.com>
2020-07-14 23:12:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,10 @@
-def line(number)
-  if line.length == 0
+def line(deli)
+  if deli.length == 0
     puts "The line is currently empty."
+  else 
+    current_line = "The line is currently:"
+    deli.each.index(1) do |person, i|
+      current_line << "#{i}. #{person}"
   end
+  puts current_line
 end
